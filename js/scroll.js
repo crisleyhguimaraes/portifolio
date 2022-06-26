bc264881fb7b8ecdeef6542ad3494e747a06b8bd
crisleyhguimaraes <crisleyhguimaraes@gmail.com>
2022-06-26 16:45:44 -0300

    adicao de script de scroll

---
+++ js/scroll.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.home', {duration : 3000, origin: 'right'})
+sr.reveal('.about', {duration : 3000, origin: 'left'})
+sr.reveal('.projects', {duration : 3000,  origin: 'left'})
+sr.reveal('.skills', {duration : 3000,  origin: 'left'})
+sr.reveal('.contact', { duration: 3000, origin: 'bottom' })
+sr.reveal('.icons', {duration : 3000, origin:'bottom'})
+sr.reveal('.column rigth', { duration: 3000, origin: 'bottom' })
+sr.reveal('.footer', {duration : 3000, origin:'bottom'})
